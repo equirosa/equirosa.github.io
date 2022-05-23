0e92ad8cb1cc6682e809a59e83e40dcb9df369f0
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-05-22 22:38:40 -0600

    quote flake parameters

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kiri's Blog";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
