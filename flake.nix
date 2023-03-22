574345c3e12046936601f8521faf4b3a047ff16d
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-21 21:29:02 -0600

    update flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,13 @@
 {
   description = "My blog repo!";
 
-  inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let pkgs = import nixpkgs {
-          inherit system;
-
-          overlays = [ devshell.overlay ];
-        };
-        in
-        pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-        };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShells.default = import ./shell.nix {inherit pkgs;};
+      formatter = nixpkgs.legacyPackages.${system}.treefmt;      
     });
 }
